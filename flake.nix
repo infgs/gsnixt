09f029ef4c87556c3a400e264370ddc0df7d8b08
rtk <r@faretek.dev>
2026-04-06 10:52:43 +0100

    feat: goboscript devshell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "goboscript nix shell thing";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    goboscript = {
+      url = "github:aspizu/goboscript";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, goboscript }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in rec {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = [
+        pkgs.hello
+        goboscript.packages.${system}.goboscript
+
+      ];
+    };
+  };
+   
+}
